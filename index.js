c2709a6ed3b03f289f99fff91fe0ee0d081b3010
Brahma behera <beherabrahmananda77@gmail.com>
2024-03-20 00:49:59 +0530

    updated code stuct cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(cors());
 
 const mongoURI = process.env.MONGO_URL;
 const conn = mongoose.createConnection(mongoURI, {
