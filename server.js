2bbb31cea6b5d34538cce8facdf0290f0bab9afe
Brahma behera <beherabrahmananda77@gmail.com>
2025-03-01 09:14:51 +0530

    removed swagger

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { initializeGridFS } from "./config/gridfs.js";
-import swaggerDocs from './utils/swagger.js'
 
 const PORT = process.env.PORT || 6001;
 
@@ -12,5 +11,4 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
   });
-  swaggerDocs(app, PORT);
 });
